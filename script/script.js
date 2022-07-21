db15a9806426290a3ad518c2059685110122b0e4
Perestra <danilo.ps.1996@gmai.com>
2022-07-20 21:26:43 -0300

    Script for mobile menu

---
+++ script/script.js
@@ -0,0 +1,25 @@
+// Active mode for Left menu mobile
+const mainNav = document.querySelector('.full_nav')
+
+function addMenuMainNav() {
+    mainNav.classList.add('active'); //Add an 'Active' class for open the menu
+}
+function RemoveMenuMainNav() {
+    mainNav.classList.remove('active'); //Remove an 'Active' class for open the menu
+}
+
+// Active mode for right menu mobile
+const header = document.querySelector('.header')
+
+function addMenuHeader() {
+    header.classList.add('active'); //Add an 'Active' class for open the menu
+}
+function removeMenuHeader() {
+    header.classList.remove('active'); //Remove an 'Active' class for open the menu
+}
+
+
+
+
+
+
