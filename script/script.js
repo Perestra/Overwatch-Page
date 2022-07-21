b4508231acc8621c11cf555ea79a94875392698f
Perestra <danilo.ps.1996@gmai.com>
2022-07-21 00:07:48 -0300

    Added a function that add a class into the body

--- script/script.js
+++ script/script.js
@@ -1,22 +1,46 @@
-// Active mode for Left menu mobile
 const mainNav = document.querySelector('.full_nav')
+const header = document.querySelector('.header')
+const body = document.querySelector('body')
 
+
+// Active mode for Left menu mobile
 function addMenuMainNav() {
-    mainNav.classList.add('active'); //Add an 'Active' class for open the menu
+    mainNav.classList.add('active'); //Add an 'Active' class to open the menu
+    validateActive()
 }
 function RemoveMenuMainNav() {
-    mainNav.classList.remove('active'); //Remove an 'Active' class for open the menu
+    mainNav.classList.remove('active'); //Remove an 'Active' class to open the menu
+    validateActive()
 }
+
 
 // Active mode for right menu mobile
-const header = document.querySelector('.header')
-
 function addMenuHeader() {
-    header.classList.add('active'); //Add an 'Active' class for open the menu
+    header.classList.add('active'); //Add an 'Active' class to open the menu
+    validateActive()
 }
 function removeMenuHeader() {
-    header.classList.remove('active'); //Remove an 'Active' class for open the menu
+    header.classList.remove('active'); //Remove an 'Active' class to open the menu
+    validateActive()
 }
+
+
+function validateActive() {
+
+    const headerClasse = header.classList.contains('active');
+    const mainNavClasse = mainNav.classList.contains('active');
+    
+    function bodyActive(){
+        if(headerClasse == true || mainNavClasse == true) {
+            body.classList.add('lock-scroll');
+        } else if(headerClasse == false && mainNavClasse == false) {
+            body.classList.remove('lock-scroll');
+        } else body.classList.remove('lock-scroll');
+    }   
+
+    bodyActive()
+}
+
 
 
 
