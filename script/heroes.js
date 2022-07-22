615b4d3e7c49bf25d352c7e3fc0c87f600f52e21
Perestra <danilo.ps.1996@gmai.com>
2022-07-22 20:31:20 -0300

    Created a new script to fit the heroes array

---
+++ script/heroes.js
@@ -0,0 +1,248 @@
+
+
+var Heroes = [
+    {
+        name: 'Ana',
+        category: 'Support',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/ana/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/ana/hero-select-portrait.png'
+    },
+    {
+        name: 'Ashe',
+        category: 'Damage',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/ashe/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/ashe/hero-select-portrait.png'
+    },
+    {
+        name: 'Baptiste',
+        category: 'Support',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/baptiste/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/baptiste/hero-select-portrait.png'
+    },
+    {
+        name: 'Bastion',
+        category: 'Damage',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/bastion/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/bastion/hero-select-portrait.png'
+    },
+    {
+        name: 'Brigitte',
+        category: 'Support',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/brigitte/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/brigitte/hero-select-portrait.png'
+    },
+    {
+        name: 'Cassidy',
+        category: 'Damage',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/cassidy/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/cassidy/hero-select-portrait.png'
+    },
+    {
+        name: 'D.Va',
+        category: 'Tank',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/dva/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/dva/hero-select-portrait.png'
+    },
+    {
+        name: 'Doomfist',
+        category: 'Damage',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/doomfist/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/doomfist/hero-select-portrait.png'
+    },
+    {
+        name: 'Echo',
+        category: 'Damage',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/echo/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/echo/hero-select-portrait.png'
+    },
+    {
+        name: 'Genji',
+        category: 'Damage',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/genji/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/genji/hero-select-portrait.png'
+    },
+    {
+        name: 'Hanzo',
+        category: 'Damage',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/hanzo/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/hanzo/hero-select-portrait.png'
+    },
+    {
+        name: 'Junkrat',
+        category: 'Damage',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/junkrat/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/junkrat/hero-select-portrait.png'
+    },
+    {
+        name: 'Lúcio',
+        category: 'Support',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/lucio/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/lucio/hero-select-portrait.png'
+    },
+    {
+        name: 'Mei',
+        category: 'Damage',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/mei/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/mei/hero-select-portrait.png'
+    },
+    {
+        name: 'Mercy',
+        category: 'Support',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/mercy/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/mercy/hero-select-portrait.png'
+    },
+    {
+        name: 'Moira',
+        category: 'Support',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/moira/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/moira/hero-select-portrait.png'
+    },
+    {
+        name: 'Orisa',
+        category: 'Tank',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/orisa/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/orisa/hero-select-portrait.png'
+    },
+    {
+        name: 'Pharah',
+        category: 'Damage',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/pharah/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/pharah/hero-select-portrait.png'
+    },
+    {
+        name: 'Reaper',
+        category: 'Damage',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/reaper/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/reaper/hero-select-portrait.png'
+    },
+    {
+        name: 'Reinhardt',
+        category: 'Tank',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/reinhardt/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/reinhardt/hero-select-portrait.png'
+    },
+    {
+        name: 'Roadhog',
+        category: 'Tank',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/roadhog/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/roadhog/hero-select-portrait.png'
+    },
+    {
+        name: 'Sigma',
+        category: 'Tank',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/sigma/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/sigma/hero-select-portrait.png'
+    },
+    {
+        name: 'Soldado: 76',
+        category: 'Damage',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/soldier-76/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/soldier-76/hero-select-portrait.png'
+    },
+    {
+        name: 'Sombra',
+        category: 'Damage',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/sombra/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/sombra/hero-select-portrait.png'
+    },
+    {
+        name: 'Symmetra',
+        category: 'Damage',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/symmetra/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/symmetra/hero-select-portrait.png'
+    },
+    {
+        name: 'Torbjörn',
+        category: 'Damage',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/torbjorn/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/torbjorn/hero-select-portrait.png'
+    },
+    {
+        name: 'Tracer',
+        category: 'Damage',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/tracer/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/tracer/hero-select-portrait.png'
+    },
+    {
+        name: 'Widowmaker',
+        category: 'Damage',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/widowmaker/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/widowmaker/hero-select-portrait.png'
+    },
+    {
+        name: 'Winston',
+        category: 'Tank',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/winston/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/winston/hero-select-portrait.png'
+    },
+    {
+        name: 'Wrecking Ball',
+        category: 'Tank',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/wrecking-ball/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/wrecking-ball/hero-select-portrait.png'
+    },
+    {
+        name: 'Zarya',
+        category: 'Tank',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/zarya/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/zarya/hero-select-portrait.png'
+    },
+    {
+        name: 'Zenyatta',
+        category: 'Support',
+        fullBodyURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/zenyatta/full-portrait.png',
+        iconURL: 'https://d1u1mce87gyfbn.cloudfront.net/hero/zenyatta/hero-select-portrait.png'
+    }
+];
+
+
+var Tank = Heroes.filter(heroTank => heroTank.category === 'Tank');
+var Support = Heroes.filter(heroSupport => heroSupport.category === 'Support');
+var Damage = Heroes.filter(heroDamage => heroDamage.category === 'Damage');
+
+const tankButton = document.querySelector('.heroe_tank')
+const supportButton = document.querySelector('.heroe_support')
+const damageButton = document.querySelector('.heroe_damage')
+
+
+const filterCharacters = function(list) {
+    
+    tankButton.classList.remove('active')
+    supportButton.classList.remove('active')
+    damageButton.classList.remove('active')
+
+    this.classList.add('active')
+
+    const iconHeroes = document.querySelector('.icon_heroes')
+    iconHeroes.innerHTML = ''
+    list.forEach(createHeroeIcon)
+}
+
+tankButton.onclick = filterCharacters.bind(tankButton, Tank)
+supportButton.onclick = filterCharacters.bind(supportButton, Support)
+damageButton.onclick = filterCharacters.bind(damageButton, Damage)
+
+const allHeroes = Heroes
+
+function createHeroeIcon(heroe) {
+      
+    const iconHeroes = document.querySelector('.icon_heroes')
+    
+    const div = document.createElement('div')
+    div.classList.add('icon_heroe_name')
+
+    var img = document.createElement('img')
+    img.setAttribute('src', heroe.iconURL)
+    var span = document.createElement('span')
+    span.innerHTML = heroe.name
+
+    div.appendChild(img)
+    div.appendChild(span)
+    iconHeroes.appendChild(div)
+}
+
+
+function listHeroes() {
+   allHeroes.forEach(createHeroeIcon)
+}
+listHeroes()
