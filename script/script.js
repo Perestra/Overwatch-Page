b665c0dbda80be1c3a3e63869118742d615a0132
Perestra <danilo.ps.1996@gmai.com>
2022-07-25 21:38:26 -0300

    Mobile menus finished

--- script/script.js
+++ script/script.js
@@ -3,8 +3,11 @@ const header = document.querySelector('.header')
 const body = document.querySelector('body')
 
 
+
 // Active mode for left menu mobile
 function addMenuMainNav() {
+    header.classList.remove('active');
+    
     mainNav.classList.add('active'); //Add an 'Active' class to open the menu
     window.scrollTo({top:0})
     body.classList.add('lock-scroll');
@@ -14,9 +17,10 @@ function RemoveMenuMainNav() {
     body.classList.remove('lock-scroll');
 }
 
-
 // Active mode for right menu mobile
 function addMenuHeader() {
+    mainNav.classList.remove('active');
+    
     header.classList.add('active'); //Add an 'Active' class to open the menu
     window.scrollTo({top:0})
     body.classList.add('lock-scroll');
